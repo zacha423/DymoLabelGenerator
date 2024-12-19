229d422d9ab151b5834d753cf881fbae7fd0a934
Zachary Abela-Gale <zachabegal@gmail.com>
2024-12-19 11:12:49 +0000

    fix typo

--- LabelGenerator/routes/index.js
+++ LabelGenerator/routes/index.js
@@ -11,8 +11,8 @@ router.get('/', function(req, res, next) {
 router.get('/test', function (req, res, next) {
   
   console.log('test');
+  const models = [];
   fs.readFile (path.join(__dirname, '../data/models.csv'), 'utf8', (err, data) => {
-    const results = [];
     if (err) {
       console.log ("Failed to read model data: " + err);
       // res.render('error',)
@@ -24,15 +24,16 @@ router.get('/test', function (req, res, next) {
 
     data.split('\n').forEach((row) => {
       model = row.split(',');
-      results.push({model:model[0],year:model[1]});
+      models.push({model:model[0],year:model[1]});
     });
 
-    console.log (results);
+    console.log (models);
 
   });
 
+  const hardwares = [];
   fs.readFile(path.join(__dirname, '../data/hardware.csv'), 'utf8', (err, data) => {
-    const results = [];
+    
 
     if (err) {
       console.log ("Failed to read hardware data: " + err);
@@ -43,16 +44,16 @@ router.get('/test', function (req, res, next) {
 
     data.split('\n').forEach ((row) => {
       hardware = row.split(',');
-      results.push({cpu:hardware[0], memory:hardware[1], storage:hardware[2]});
+      hardwares.push({cpu:hardware[0], memory:hardware[1], storage:hardware[2]});
     });
 
-    console.log (results);
+    console.log (hardwares);
   })
   
   
   
   
-  res.render ('2js', {title:'Bumpdown Label Template'});
+  res.render ('2js', {title:'Bumpdown Label Template', models:models, hardware:hardwares});
 
   
 });

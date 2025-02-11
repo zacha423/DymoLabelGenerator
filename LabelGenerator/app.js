a12e03df939b26f19fc62b57005cc1ecfb91b9fd
Zachary Abela-Gale <zachabegal@gmail.com>
2025-02-11 18:36:45 +0000

    General code cleanup

--- LabelGenerator/app.js
+++ LabelGenerator/app.js
@@ -3,9 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
-const indexRouter = require('./routes/index');
-
+const labelRouter = require('./routes/labelRouter');
+const printerRouter = require('./routes/printerRouter');
 const app = express();
 
 // view engine setup
@@ -18,7 +17,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', labelRouter);
+app.use('/', printerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- LabelGenerator/controllers/printerController.js
+++ LabelGenerator/controllers/printerController.js
@@ -1,24 +1,33 @@
 import fetch from 'node-fetch';
 import {parseString} from 'xml2js';
+import {createRequire } from 'module';
+import DYMO_HOSTNAME from '../constants';
+const esmRequire = createRequire (import.meta.url);
+const Dymo = esmRequire('dymojs');
+const dymo = new Dymo({hostname: DYMO_HOSTNAME});
+
 
 async function loadPrinters () {
-  const rawPrinter = await fetch('https://host.docker.internal:41951/DYMO/DLS/Printing/GetPrinters');
-  let resu = await rawPrinter.text();
+  const rawPrinter = await fetch('https://' + DYMO_HOSTNAME + ':41951/DYMO/DLS/Printing/GetPrinters');
+  let printersXML = await rawPrinter.text();
 
-  parseString (resu, (err, res) => {
-    resu = res;
+  parseString (printersXML, (err, res) => {
+    printersXML = res;
   })
 
-  // get array of printer names
-  console.log("printers loaded");
-  return resu['Printers']['LabelWriterPrinter'].flatMap((data => data.Name))
+  console.log("Startup: Loaded Printers!");
+  return printersXML['Printers']['LabelWriterPrinter'].flatMap((printer => printer.Name))
 }
 
-let printe2rs;
-loadPrinters().then(data => {printe2rs = data});
+let availablePrinters;
+loadPrinters().then(data => {availablePrinters = data});
 
 
 export function printers(req, res, next) {
-  console.log('test');
-  res.send (printe2rs);
+  res.send (availablePrinters);
+}
+
+export function print (req, res, next) {
+  dymo.print(req.body.printer, req.body.label);
+  res.status(200).send();
 }
